93470ced0b144eea4bbb16d54f425fc557f30a71
ArchanjoJr <math.calazans56@gmail.com>
2018-08-14 18:43:50 -0300

    finish exercices 1 and 2

--- src/exercicio1/App.java
+++ src/exercicio1/App.java
@@ -1,11 +1,25 @@
 package exercicio1;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		int a,b;
-		System.out.println();
-
+		Scanner read = new Scanner(System.in);
+		System.out.print("Digite o primeiro numero: ");
+		a = read.nextInt();
+		System.out.print("Digite o Segundo Numero: ");
+		b = read.nextInt();
+		// Exercicio 1 LISTA 2
+		System.out.printf("SOMA: %d \nSUBTR: %d \nMULTI: %d \nDIVIS: %.2f",(a+b), (a-b), (a*b), ((float) a/b));
+		// EXERCICIO 2 LISTA 2
+		System.out.printf("SOMA: %d \nMEDIA: %d\n",(a+b), (a+b)/2);
+		if (a > b) {
+			System.out.printf("%d, %d", a, b);
+		} else {
+			System.out.printf("%d, %d", b, a);
+		}
 	}
 
 }
